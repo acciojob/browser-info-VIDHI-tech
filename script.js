24539adabd0ea4ad6399a32879dd7d40b58a7e80
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-01 14:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const info = document.getElementById("browser-info");
+
+function getInfo() {
+	const browserName = navigator.userAgent;
+	const version = navigator.appVersion;
+
+	console.log("You are using " + browserName + "version " + version);
+}
